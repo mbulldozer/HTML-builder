7a00f7f267f3f80cd8178cb524b874a2cd33ecf5
Aliaksandr Pauliukavets <al.pavlyukovets@yandex.ru>
2022-05-13 19:35:42 +0300

    feat: solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const fs = require('fs');
+const path = require('node:path');
 
+const folderPath = path.resolve('04-copy-directory/files');
+const copyFolderPath = `${folderPath}-copy`;
+
+fs.mkdir(copyFolderPath, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(copyFolderPath, {withFileTypes: true}, (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+      const pathToFile = `${copyFolderPath}/${file.name}`;
+      fs.unlink(pathToFile, (err) => {
+        if (err) throw err;
+      });
+    });
+  }
+});
+
+fs.readdir(folderPath, {withFileTypes: true}, (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+      const pathToFile = `${folderPath}/${file.name}`;
+      const newPathToFile = `${copyFolderPath}/${file.name}`;
+      fs.stat(pathToFile, (err, stats) => {
+        if (stats.isFile()) {
+          fs.copyFile(pathToFile, newPathToFile, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    });
+  }
+});
